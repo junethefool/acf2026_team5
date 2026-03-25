8057c2df4c8668221e68000525f94fc3b4b93dc8
LouisoftheStars <Louis.behrmann@gmail.com>
2026-03-25 16:48:28 +0100

    room transition system

--- scripts/pickupable.gd
+++ scripts/pickupable.gd
@@ -1,3 +1,16 @@
 extends AnimatableBody3D
 
 var pickupable = true
+var spawn: Vector3
+var locked = false
+func _ready():
+	call_deferred("_ready2")
+
+func _ready2():
+	spawn = global_position
+	for room_transition in get_tree().get_nodes_in_group("room_transitions"):
+		room_transition.room_transition.connect(_on_room_transition)
+		
+func _on_room_transition():
+	if $Sprite3D.visible == true and locked != true:
+		global_position = spawn
---
+++ scripts/room_transition.gd
@@ -0,0 +1,22 @@
+extends Area3D
+
+signal room_transition
+
+func _ready():
+	body_entered.connect(_on_body_entered)
+	add_to_group("room_transitions")
+	
+	 
+func _on_body_entered(body: CharacterBody3D):
+	
+	if body.name == "PlayerCharacter":
+		room_transition.emit()
+	
+	if body.current_pick_up != null:
+		body.state = 0
+		for child in body.current_pick_up.get_children():
+			if child is CollisionShape3D:
+				child.disabled = false
+		body.current_pick_up = null
+	
+	body.global_position = $Marker3D.global_position
