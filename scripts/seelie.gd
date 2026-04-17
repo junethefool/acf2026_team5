6f39473d0a89f4fb9cac667ae2ec4414cc5ce660
LouisoftheStars <Louis.behrmann@gmail.com>
2026-04-17 23:38:46 +0200

    progress

--- scripts/seelie.gd
+++ scripts/seelie.gd
@@ -7,6 +7,7 @@ extends AnimatableBody3D
 func _ready() -> void:
 	interactable.Interact.connect(on_interact)
 	Dialogic.signal_event.connect(on_dialogic_signal)
+	$Area3D.body_entered.connect(on_body_entered)
 
 func on_interact():
 	print("you did it, dumbass")
@@ -18,7 +19,7 @@ func on_dialogic_signal(message):
 		var duration = 0.5
 		var wait = 0.5
 		await get_tree().create_timer(duration).timeout
-		crow.global_position = $next_to_seelie.global_position
+		crow.global_position = $crow.global_position
 		await get_tree().create_timer(wait + duration).timeout
 		Dialogic.start("seelie")
 		
@@ -33,3 +34,16 @@ func on_dialogic_signal(message):
 		get_tree().get_first_node_in_group("player").global_position = $player_position.global_position
 		await get_tree().create_timer(wait + duration).timeout
 		Dialogic.start("crow")
+	
+	elif message == "come here":
+		get_tree().get_first_node_in_group("fade").fade(0.5,0.5)
+		var duration = 0.5
+		var wait = 0.5
+		await get_tree().create_timer(duration).timeout
+		get_tree().get_first_node_in_group("player").global_position = $talk.global_position
+		await get_tree().create_timer(wait + duration).timeout
+		Dialogic.start("seelie")
+func on_body_entered(body):
+	if Dialogic.VAR.first_talk == false and body is CharacterBody3D:
+		
+		Dialogic.start("seelie")
